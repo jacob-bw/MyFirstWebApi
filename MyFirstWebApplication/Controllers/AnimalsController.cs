c40a937bcd72b7a9d095b861fe8f196a7d7eaa74
Jacob <wittenberg.jacob@gmail.com>
2020-04-01 20:01:54 -0500

    Add project files.

---
+++ MyFirstWebApplication/Controllers/AnimalsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApi.Models;
+
+namespace MyFirstWebApi.Controllers
+{
+    [Route("api/animals")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllAnimals()
+        {
+            var animals = new List<Animal>
+            {
+                new Animal {Name = "Steve", Type = "Elephant", Weight = 2000},
+                new Animal {Name = "George", Type = "Monkey", Weight = 60},
+                new Animal {Name = "Bill", Type = "Zebroid", Weight = 300},
+            };
+            return Ok(animals);
+        }
+
+
+
+    }
+}
---
+++ MyFirstWebApplication/Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApi.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Weight { get; set; }
+    }
+}
